b6abec535219981a7b50413b788daf0f1b68c071
rychtarikj <rychtarikj@example.com>
2025-08-11 13:52:52 +0200

    backup-08

--- src/components/FormAdd.jsx
+++ src/components/FormAdd.jsx
@@ -21,77 +21,73 @@ function FormAdd({ handleAddProg }) {
   };
 
   return (
-    <div>
+    <div className="container-fluid">
       {/* formular pro pridani programatora do team */}
 
-      <div className="container">
-        <div className="row">
-          <form onSubmit={handleSubmit}>
-            <fieldset className="border rounded p-3">
-              <legend>Přidání nového programátora</legend>
-              <div className="row">
-                {/* input jmeno */}
-                <div className="col d-flex align-items-center gap-3">
-                  <label htmlFor="name" className="form-label">
-                    Name:
-                  </label>
-                  <input
-                    type="text"
-                    className="form-control"
-                    name="name"
-                    id="name"
-                    aria-describedby="name"
-                    placeholder=""
-                    onChange={handleNameChange}
-                    value={progName}
-                  />
-                </div>
-                {/* level radio */}
-                <div className="col d-flex align-items-center gap-3">
-                  <div className="form-check">
-                    <input
-                      className="form-check-input"
-                      type="radio"
-                      name="level"
-                      id="radioJunior"
-                      value="junior"
-                      checked={levelCheck === "junior"}
-                      onChange={() => setLevelCheck("junior")}
-                    />
-                    <label className="form-check-label" htmlFor="radioJunior">
-                      Junior
-                    </label>
-                  </div>
-                  <div className="form-check">
-                    <input
-                      className="form-check-input"
-                      type="radio"
-                      name="level"
-                      id="radioSenior"
-                      value="senior"
-                      checked={levelCheck === "senior"}
-                      onChange={() => setLevelCheck("senior")}
-                    />
-                    <label className="form-check-label" htmlFor="radioSenior">
-                      Senior
-                    </label>
-                  </div>
-
-                  {/* add button */}
-                  <button
-                    type="submit"
-                    className="btn btn-primary"
-                    name="addProg"
-                    id="addProg"
-                  >
-                    Add
-                  </button>
-                </div>
+      <form onSubmit={handleSubmit}>
+        <fieldset className="border rounded p-3">
+          <legend>Přidání nového programátora</legend>
+          <div className="row">
+            {/* input jmeno */}
+            <div className="col d-flex align-items-center gap-3">
+              <label htmlFor="name" className="form-label">
+                Name:
+              </label>
+              <input
+                type="text"
+                className="form-control"
+                name="name"
+                id="name"
+                aria-describedby="name"
+                placeholder=""
+                onChange={handleNameChange}
+                value={progName}
+              />
+            </div>
+            {/* level radio */}
+            <div className="col d-flex align-items-center gap-3">
+              <div className="form-check">
+                <input
+                  className="form-check-input"
+                  type="radio"
+                  name="level"
+                  id="radioJunior"
+                  value="junior"
+                  checked={levelCheck === "junior"}
+                  onChange={() => setLevelCheck("junior")}
+                />
+                <label className="form-check-label" htmlFor="radioJunior">
+                  Junior
+                </label>
               </div>
-            </fieldset>
-          </form>
-        </div>
-      </div>
+              <div className="form-check">
+                <input
+                  className="form-check-input"
+                  type="radio"
+                  name="level"
+                  id="radioSenior"
+                  value="senior"
+                  checked={levelCheck === "senior"}
+                  onChange={() => setLevelCheck("senior")}
+                />
+                <label className="form-check-label" htmlFor="radioSenior">
+                  Senior
+                </label>
+              </div>
+
+              {/* add button */}
+              <button
+                type="submit"
+                className="btn btn-primary"
+                name="addProg"
+                id="addProg"
+              >
+                Add
+              </button>
+            </div>
+          </div>
+        </fieldset>
+      </form>
     </div>
   );
 }
